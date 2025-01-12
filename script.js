2a4f5d714838cc0ee92b59a004bad1c33ef65375
Steffen <74921589+leneffets@users.noreply.github.com>
2025-01-12 07:18:23 +0000

    format lastUpdate timestamp

--- script.js
+++ script.js
@@ -58,7 +58,15 @@ async function updateExchangeRate() {
         const data = await response.json();
         exchangeRate = data.rates[targetCurrency];
         const exchangeTimestamp = data.time_last_updated
-                    ? new Date(data.time_last_updated * 1000).toLocaleString()
+                    ? new Date(data.time_last_updated * 1000).toLocaleString("de-DE", {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit',
+                        hour12: false
+                    })
                     : 'Unknown';
 
         // Save to local storage
